5f7835a8ff5123d5600e4aa2daf700b42c5df2bc
tlapata <111437411+tlapata@users.noreply.github.com>
2024-09-30 23:38:27 +0200

    Added render temp domain to CORS rules.

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
-  'http://localhost:3003'
+  'http://localhost:3003',
+  'https://escortmundo-com.onrender.com/'
 ];
 app.use(cors({
   origin: (origin, callback) => {
